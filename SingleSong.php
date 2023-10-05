b9808e526229565cca60553df7b20c3b514c2c51
Joe Masri <zelma760@mtroyal.ca>
2023-10-05 12:08:32 -0600

    setting up layout single song page

--- SingleSong.php
+++ SingleSong.php
@@ -1,1 +1,40 @@
-//Single Song page
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>COMP 3512 Assign1</title>
+</head>
+<body>
+    <header>
+        <h1>Header</h1>
+        <h4>Zee El Masri, Andrew Yu</h4>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Browse</a></li>
+            <li><a href="#">Search</a></li>
+            <li><a href="#">About Us</a></li>
+        </ul>
+    </nav>
+
+    <section>
+        <h1>Song Information</h1>
+        <!-- Your song information content goes here -->
+        <p>title, artist name, artist type, genre, year, duration</p>
+        <p>Analysis Data</p>
+        <ul>
+            <li>bpm,</li>
+            <li>energy,</li>
+            <li>danceability,</li>
+            <li>liveness,</li>
+
+        </ul>
+    </section>
+
+    <footer>
+        <h2>Footer</h2>
+    </footer>
+</body>
+</html>
