85e442c19850e7eea4b93f8fef169d627d13921a
ayu381 <ayu381@mtroyal.ca>
2023-10-08 15:06:51 -0600

    Got some info displayed

--- SingleSong.php
+++ SingleSong.php
@@ -1,3 +1,29 @@
+<?php
+
+$db = new PDO('sqlite:./data/music.db');
+
+try {
+    $sql = 'SELECT * FROM songs WHERE song_id=:si';
+    
+    //statement is prepred
+    $statement= $db->prepare($sql);
+
+    //value is retrieved from querystring
+    $song_id = filter_input(INPUT_GET, 'song_id');
+    $statement->bindValue(':si', $song_id, PDO::PARAM_INT);
+
+    //query is executed
+    $statement->execute();
+
+    //array of songs are made
+    $s = $statement->fetch();
+    $db = null;
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -23,15 +49,19 @@
     <section>
         <h1>Song Information</h1>
         <!-- Your song information content goes here -->
-        <p>title, artist name, artist type, genre, year, duration</p>
-        <p>Analysis Data</p>
+        
+        <!--verify contents of database-->
+        <?php if ($s) : ?>
+        <p><?php echo htmlspecialchars ($s['title']); ?>, </p>
         <ul>
-            <li>bpm,</li>
-            <li>energy,</li>
-            <li>danceability,</li>
-            <li>liveness,</li>
-
-        </ul>
+            <li><strong>BPM:</strong> <?php echo htmlspecialchars($s['bpm']); ?></li>
+            <li><strong>Energy:</strong> <?php echo htmlspecialchars($s['energy']); ?></li>
+            <li><strong>Danceability:</strong> <?php echo htmlspecialchars($s['danceability']); ?></li>
+            <li><strong>Loudness:</strong> <?php echo htmlspecialchars($s['loudness']); ?></li>
+        <?php else : ?>
+            <li>No song found</li>
+        <?php endif; ?>
+    </ul>
     </section>
 
     <footer>
@@ -47,7 +77,5 @@
             <a class="footer-link" href="https://github.com/ayu381">Group Member 2</a>
         </p>
     </footer>
-
-
 </body>
 </html>
